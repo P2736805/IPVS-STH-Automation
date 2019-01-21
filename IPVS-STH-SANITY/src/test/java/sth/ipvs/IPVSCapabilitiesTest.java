aee269a62767815a515fec7d4957f83d0864ded4
426525 <426525@192.168.175.85>
2019-01-21 18:35:01 +0530

    Charter CGUID Changes done

--- IPVS-STH-SANITY/src/test/java/sth/ipvs/IPVSCapabilitiesTest.java
+++ IPVS-STH-SANITY/src/test/java/sth/ipvs/IPVSCapabilitiesTest.java
@@ -24,7 +24,7 @@ public class IPVSCapabilitiesTest extends IPVSBaseTest {
 	public void ipvscapabilitiescallCHTR() {
 		JSON response = execute().path()
 								 .piauthentication()
-								 .query("customerGuid", "51585793")
+								 .query("customerGuid", "44188796")
 								 .query("ip", "1.1.1.1")
 								 .query("mso", "CHARTER")
 								 .query("requestId", "STHCapabilitiesCheck-Charter")
